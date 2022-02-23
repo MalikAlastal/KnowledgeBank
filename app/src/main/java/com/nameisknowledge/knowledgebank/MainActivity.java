31d9d1bc1fd499b6e08bb79f8c48a138f8eda321
MalikAlastal <maworklik@gmail.com>
2022-02-23 13:30:54 +0200

    love push

--- app/src/main/java/com/nameisknowledge/knowledgebank/MainActivity.java
+++ app/src/main/java/com/nameisknowledge/knowledgebank/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String firstText = "Abood is here" ;
         String malik = "malik old" ;
+        String you = "i cant love you " ;
     }
 }
