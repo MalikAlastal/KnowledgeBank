07934c05a0e2aa2e1503a5b154f7d8886d18f652
aboodemad61 <xhaka70224@gmail.com>
2022-02-23 13:38:08 +0200

    tst

--- app/src/main/java/com/nameisknowledge/knowledgebank/MainActivity.java
+++ app/src/main/java/com/nameisknowledge/knowledgebank/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String firstText = "Abood is here" ;
         String malik = "malik old" ;
+        int age = 10;
     }
 }
