1d2e9f33cf17c442a355201e87101b5504423dc2
aboodemad61 <xhaka70224@gmail.com>
2022-02-28 15:16:55 +0200

    My Feature

--- app/src/main/java/com/nameisknowledge/knowledgebank/TestDialog.java
+++ app/src/main/java/com/nameisknowledge/knowledgebank/TestDialog.java
@@ -12,28 +12,26 @@ import androidx.fragment.app.DialogFragment;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TestDialog extends DialogFragment {
-    private String DocumentId;
-    public static TestDialog newInstance(String id) {
+    private static DocumentReference documentId;
+    public static TestDialog newInstance(DocumentReference documentReference) {
         TestDialog fragment = new TestDialog();
         Bundle args = new Bundle();
-        args.putString("id",id);
+        documentId = documentReference;
         fragment.setArguments(args);
         return fragment;
     }
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        if (getArguments() != null) {
-            DocumentId = getArguments().getString("id");
-        }
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("Request")
                 .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        FirebaseFirestore.getInstance().collection("Requests").document().collection("container").document(DocumentId).update("Request","accepted").addOnSuccessListener(new OnSuccessListener<Void>() {
+                        documentId.update("Request","accepted").addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
                                 dismiss();
@@ -48,7 +46,7 @@ public class TestDialog extends DialogFragment {
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        FirebaseFirestore.getInstance().collection("Requests").document().collection("container").document(DocumentId).update("Request","canceled").addOnSuccessListener(new OnSuccessListener<Void>() {
+                        documentId.update("Request","canceled").addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
                                 dismiss();
