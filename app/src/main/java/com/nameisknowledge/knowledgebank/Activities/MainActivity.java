82bfc8ee8f83bd25d5ebc68ab926758a5fe562f7
aboodemad61 <xhaka70224@gmail.com>
2022-02-28 14:20:33 +0200

    My Feature

--- app/src/main/java/com/nameisknowledge/knowledgebank/Activities/MainActivity.java
+++ app/src/main/java/com/nameisknowledge/knowledgebank/Activities/MainActivity.java
@@ -1,21 +1,71 @@
 package com.nameisknowledge.knowledgebank.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 import com.nameisknowledge.knowledgebank.R;
 import com.nameisknowledge.knowledgebank.databinding.ActivityMainBinding;
 
+import java.util.List;
+import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding ;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        binding.clickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+            }
+        });
+
+        FirebaseFirestore.getInstance().collection("Requests").document(Objects.requireNonNull(FirebaseAuth.getInstance().getUid())).collection("container").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error == null){
+                    if (value!=null){
+                        for (DocumentChange documentChange:value.getDocumentChanges()){
+                            switch (documentChange.getType()){
+                                case ADDED:
+                                    Log.d("AddChange",documentChange.getDocument().getData().toString());
+                                    break;
+                                case MODIFIED:
+                                    Log.d("UpdateChange",documentChange.getDocument().getData().toString());
+                                    break;
+                                case REMOVED:
+                                    Log.d("RemoveChange",documentChange.getDocument().getData().toString());
+                                    break;
+                            }
+                        }
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/nameisknowledge/knowledgebank/ReqListener.java
@@ -0,0 +1,7 @@
+package com.nameisknowledge.knowledgebank;
+
+import com.nameisknowledge.knowledgebank.ModelClasses.UserMD;
+
+public interface ReqListener {
+    void onClick(UserMD userMD);
+}
