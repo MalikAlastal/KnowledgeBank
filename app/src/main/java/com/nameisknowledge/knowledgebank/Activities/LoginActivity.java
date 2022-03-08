bbb773fa73c6d7f58ea30f19a0cae029dcec47cf
aboodemad61 <xhaka70224@gmail.com>
2022-03-08 15:40:26 +0200

    solo method

--- app/src/main/java/com/nameisknowledge/knowledgebank/Activities/LoginActivity.java
+++ app/src/main/java/com/nameisknowledge/knowledgebank/Activities/LoginActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -13,12 +15,15 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.nameisknowledge.knowledgebank.ModelClasses.UserMD;
 import com.nameisknowledge.knowledgebank.databinding.ActivityLoginBinding;
 
+import java.util.Calendar;
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
+    public final static String[] keys = {"email","password"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +33,22 @@ public class LoginActivity extends AppCompatActivity {
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(binding.edEmail.getText().toString(),binding.edPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        startActivity(new Intent(getBaseContext(),MainActivity.class));
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+                try {
+                    checkEmpty(setDataForKeyAndValue(keys,new EditText[]{binding.edEmail,binding.edPassword}));
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(binding.edEmail.getText().toString(),binding.edPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            startActivity(new Intent(getBaseContext(),MainActivity.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (Exception e) {
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -48,5 +58,52 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void checkEmpty(KeyAndValue[] keyAndValues) throws Exception {
+        for (KeyAndValue keyAndValue:keyAndValues){
+            if (keyAndValue.getValue().getText().toString().isEmpty()){
+                throw new Exception(keyAndValue.getKey()+"Is Empty");
+            }
+        }
+    }
+
+    private KeyAndValue[] setDataForKeyAndValue(String[] keys,EditText[] values){
+        KeyAndValue[] keyAndValues = new KeyAndValue[keys.length];
+        for (int i=0;i<values.length;i++){
+            keyAndValues[i] = new KeyAndValue(keys[i],values[i]);
+        }
+        return keyAndValues;
+    }
+
+    private UserMD setData() throws Exception {
+        checkEmpty(setDataForKeyAndValue(keys,new EditText[]{binding.edEmail,binding.edPassword}));
+        return new UserMD("as",binding.edEmail.getText().toString(),binding.edPassword.getText().toString(),"","", Calendar.getInstance().getTime(),Calendar.getInstance().getTime());
+    }
+
+    class KeyAndValue {
+        private String key;
+        private EditText value;
+
+        public KeyAndValue(String key, EditText value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public EditText getValue() {
+            return value;
+        }
+
+        public void setValue(EditText value) {
+            this.value = value;
+        }
     }
 }
