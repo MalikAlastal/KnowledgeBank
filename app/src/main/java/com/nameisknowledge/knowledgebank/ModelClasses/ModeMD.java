3ae81cf56bee460af88be8414aed0cf09ec9e43e
MalikAlastal <maworklik@gmail.com>
2022-03-22 15:50:07 +0200

    add ModeMD

---
+++ app/src/main/java/com/nameisknowledge/knowledgebank/ModelClasses/ModeMD.java
@@ -0,0 +1,41 @@
+package com.nameisknowledge.knowledgebank.ModelClasses;
+
+public class ModeMD {
+
+    int resName;
+    int resImage;
+    int resMainColor;
+
+    public ModeMD(int resName, int resImage, int mainColor) {
+        this.resName = resName;
+        this.resImage = resImage;
+        this.resMainColor = mainColor;
+    }
+
+    public ModeMD() {
+    }
+
+    public int getResName() {
+        return resName;
+    }
+
+    public void setResName(int resName) {
+        this.resName = resName;
+    }
+
+    public int getResImage() {
+        return resImage;
+    }
+
+    public void setResImage(int resImage) {
+        this.resImage = resImage;
+    }
+
+    public int getResMainColor() {
+        return resMainColor;
+    }
+
+    public void setResMainColor(int resMainColor) {
+        this.resMainColor = resMainColor;
+    }
+}
